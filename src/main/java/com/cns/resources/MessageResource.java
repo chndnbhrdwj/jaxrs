b556d96c33dcec84e7934c421edc90b88285ce9c
Chandan Kumar <kumar.chandan@mastek.com>
2023-03-09 21:57:07 +0000

    cleanup

--- src/main/java/com/cns/resources/MessageResource.java
+++ src/main/java/com/cns/resources/MessageResource.java
@@ -1,7 +1,5 @@
 package com.cns.resources;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 import com.cns.model.Message;
@@ -17,12 +15,10 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
-import jakarta.ws.rs.core.Response.Status;
 
 @Path("/messages")
 @Consumes(MediaType.APPLICATION_JSON)
