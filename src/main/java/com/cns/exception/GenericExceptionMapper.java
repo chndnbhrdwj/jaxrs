c968e9c8d0a2acace5315524b52a2cce8e7ad28c
Chandan Kumar <kumar.chandan@mastek.com>
2023-03-09 22:24:31 +0000

    generic exception mapper

---
+++ src/main/java/com/cns/exception/GenericExceptionMapper.java
@@ -0,0 +1,20 @@
+package com.cns.exception;
+
+import com.cns.model.ErrorMessage;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Override
+    public Response toResponse(Throwable exception) {
+        ErrorMessage error = new ErrorMessage(exception.getMessage(), 500, "alink");
+        return Response.status(Status.INTERNAL_SERVER_ERROR)
+                .entity(error)
+                .build();
+    }
+}
